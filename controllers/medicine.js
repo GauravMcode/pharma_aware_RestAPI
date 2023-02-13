46c69f3862bdd6d79d3029b0f61b91638ed799e7
unknown <gauravrajanju@gmail.com>
2023-02-14 01:46:00 +0530

    config the puppeteer launch

--- controllers/medicine.js
+++ controllers/medicine.js
@@ -11,56 +11,67 @@ exports.getMedInfo = async function (req, res, next) {
     const sockets = await io.getIO().in(socketId).fetchSockets(); //get sockets of a socketId /*https://socket.io/docs/v4/server-instance/#fetchsockets*/
     const socket = sockets[0];
     // console.log('socket is connected: ' + socket.connected());
-    browser = await puppeteer.launch({ defaultViewport: false });
+    browser = await puppeteer.launch({
+        headless: true,
+        executablePath: '/usr/bin/chromium-browser',
+        args: [
+            '--no-sandbox',
+            '--disable-gpu',
+        ],
+        defaultViewport: false
+    });
     page = await browser.newPage();
     const medicineName = req.params.searchTerm;
     // const MED_NO = process.env.MED_NO;
     console.log(`Searching for: ${medicineName}`);
     let meds_1mg = [];
-    console.log('1');
-    await page.goto(`https://www.1mg.com/search/all?name=${medicineName}`, { waitUntil: 'domcontentloaded' }),
-        console.log('2');
-    console.log('3');
-    meds_1mg = await page.$$eval('div.style__horizontal-card___1Zwmt', divs => divs.map(div => {
-        return {
-            title: div.querySelector('.style__pro-title___3zxNC').textContent,
-            price: div.querySelector('.style__price-tag___B2csA').textContent,
-            imageUrl: '',
-            pageUrl: div.querySelector('a').href,
-            description: ""  //ProductDescription__description-content___A_qCZ
+    try {
+        await page.goto(`https://www.1mg.com/search/all?name=${medicineName}`, { waitUntil: 'domcontentloaded' }),
+            meds_1mg = await page.$$eval('div.style__horizontal-card___1Zwmt', divs => divs.map(div => {
+                return {
+                    title: div.querySelector('.style__pro-title___3zxNC').textContent,
+                    price: div.querySelector('.style__price-tag___B2csA').textContent,
+                    imageUrl: '',
+                    pageUrl: div.querySelector('a').href,
+                    description: ""  //ProductDescription__description-content___A_qCZ
+                }
+            }));
+        if (meds_1mg.length === 0) {
+            // style__product-box___3oEU6
+            meds_1mg = await page.$$eval('div.style__product-box___3oEU6', divs => divs.map(div => {
+                return {
+                    title: div.querySelector('.style__pro-title___3G3rr').textContent,
+                    price: div.querySelector('.style__price-tag___KzOkY').textContent,
+                    imageUrl: '',
+                    pageUrl: div.querySelector('a').href,
+                    description: ""
+                }
+            }));
         }
-    }));
-    if (meds_1mg.length === 0) {
-        // style__product-box___3oEU6
-        meds_1mg = await page.$$eval('div.style__product-box___3oEU6', divs => divs.map(div => {
-            return {
-                title: div.querySelector('.style__pro-title___3G3rr').textContent,
-                price: div.querySelector('.style__price-tag___KzOkY').textContent,
-                imageUrl: '',
-                pageUrl: div.querySelector('a').href,
-                description: ""
-            }
-        }));
-    }
-    if (!socket) {
-        console.log('Socket id undefined: Socket not connected')
-        res.redirect('/')
-        return;
-    }
-    io.getIO().to(socketId).emit('medList', meds_1mg);
-    await autoScroll(page); //scroll till half
+        if (!socket) {
+            console.log('Socket id undefined: Socket not connected')
+            res.redirect('/')
+            return;
+        }
+        io.getIO().to(socketId).emit('medList', meds_1mg);
+        await autoScroll(page); //scroll till half
 
-    // meds_1mg = meds_1mg.filter((value, index, arr) => index < MED_NO); //limit till 13 elements
-    // await page.waitForSelector('.style__loaded___22epL', { visible: true, timeout: 20000, hidden: true })
-    const images = await page.$$eval('.style__loaded___22epL', imgs => imgs.map(image => image.getAttribute('src')))
-    for (let i = 0; i < meds_1mg.length; i++) {
-        if (i >= images.length) {
-            continue;
+        // meds_1mg = meds_1mg.filter((value, index, arr) => index < MED_NO); //limit till 13 elements
+        // await page.waitForSelector('.style__loaded___22epL', { visible: true, timeout: 20000, hidden: true })
+        const images = await page.$$eval('.style__loaded___22epL', imgs => imgs.map(image => image.getAttribute('src')))
+        for (let i = 0; i < meds_1mg.length; i++) {
+            if (i >= images.length) {
+                continue;
+            }
+            meds_1mg[i].imageUrl = images[i];
         }
-        meds_1mg[i].imageUrl = images[i];
+        await browser.close();
+        res.status(200).json({ meds_1mg: meds_1mg })
     }
-    await browser.close();
-    res.status(200).json({ meds_1mg: meds_1mg })
+    catch (err) {
+        res.status(404).json({ message: 'some error occured' });
+        console.log(err);
+    }
 
     if (!socketId) {
         console.log('Socket id undefined: Socket not connected')
@@ -80,7 +91,15 @@ exports.getMedInfo = async function (req, res, next) {
 
     /* Going to new page and blocking for image  request on that page */
     try {
-        const browser1 = await puppeteer.launch({ defaultViewport: false });
+        const browser1 = await puppeteer.launch({
+            headless: true,
+            executablePath: '/usr/bin/chromium-browser',
+            args: [
+                '--no-sandbox',
+                '--disable-gpu',
+            ],
+            defaultViewport: false
+        });
         const page1 = await browser1.newPage();
         await page1.setRequestInterception(true)
         page1.on('request', (request) => {
@@ -92,7 +111,6 @@ exports.getMedInfo = async function (req, res, next) {
             await page1.goto(medLinks[i], { waitUntil: 'domcontentloaded' });
             let description;
             if (medLinks[i].includes('drugs')) {
-                console.log('drug..');
                 description = await page1.$eval('#overview > div > div > div > div', desc => desc.innerText);
             }
             else if (medLinks[i].includes('otc')) {
